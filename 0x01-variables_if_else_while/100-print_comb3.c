d659a9a4957049d5becda5ce7b9635f51b137623
Idowu Oreoluwa <idowuore2002@gmail.com>
2022-09-11 19:18:30 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - program that prints the numbers from 01 to 89
+* Numbers must be separated by , followed by a space
+* Numbers should be printed in ascending order with 2 digits
+* You can only usethe putchar function
+* Return: 0
+*/
+int main(void)
+{
+int d;
+for (d = 0; d < 90; d++)
+{
+putchar((d / 10) + '0');
+putchar((d % 10) + '0');
+if (d != 89)
+{
+putchar(',');
+putchar(',');
+}
+}
+putchar('\n');
+return (0);
+}
