e2317e5b24e1bbb438860e43c865f188688bb07b
Idowu Oreoluwa <idowuore2002@gmail.com>
2022-09-14 18:16:26 -0700

    5sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,14 @@ if (n > 0)
 _putchar('+');
 return (1);
 }
-else if (n < 0)
+else if (n == 0)
 {
-_putchar('-')
-return (-1);
+_putchar('0')
+return (0);
 }
 else
 {
-_putchar('0');
-return (0);
+_putchar('-');
+return (1);
 }
 }
