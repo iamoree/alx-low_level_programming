c281b4c786160ec5bb2ac5edfac47c186d91704a
Idowu Oreoluwa <idowuore2002@gmail.com>
2022-09-16 16:32:28 -0700

    0isup

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,11 +4,11 @@
 *@c: character to be tested
 *Return: 1 whether it is, 0 otherwise
 */
-int _isupper(int c) 
+int _isupper(int c)
 {
 if ((c >= 'A') && (c <= 'Z'))
-{    
-return (1); 
+{
+return (1);
 }
 return (0);
 }
