0703cb2f461de98c277cebf9e4ab2aac23a026c5
Idowu Oreoluwa <idowuore2002@gmail.com>
2022-09-27 13:27:52 -0700

    5-str

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,7 +2,7 @@
 /**
 * _strstr - locates a substring
 * @haystack: string in which to check for needle
-* @needle: substring to find in haystack 
+* @needle: substring to find in haystack
 *
 * Return: pointer to beginning of needle in haystack or NULL if no match
 */
