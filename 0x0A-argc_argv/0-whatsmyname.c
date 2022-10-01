027772cdd93ef0e2f5eaaaaaaa216be8188efa70
Idowu Oreoluwa <idowuore2002@gmail.com>
2022-10-01 13:25:20 -0700

    0what

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,10 @@
 * @argc: number of arguments passed to the function
 * @argv: argument vector of pointers to strings
 *
-* Return: always 0 
+* Return: always 0
 */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
 }
-
